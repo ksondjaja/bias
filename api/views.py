52d4e51176dd4bcaf088c5b689d4c1b057d02a5f
ksondjaja <sondjajaja@gmail.com>
2020-08-29 22:31:22 -0400

    Show extracted text

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django import forms
+from django.contrib import messages
 from . import util
+import urllib
 
 # Create your views here.
 
@@ -19,9 +21,11 @@ def lookup(request):
             news = article.cleaned_data["news"]
             output = util.getStuff(news)
             score = util.getBiasIndex(news)
-            return render (request, "api/bias.html",{
-                "author": output[0],
-                "title": output[1],
-                "news": news,
-                "score": score
-            })
+            if len(output)==3:
+                return render (request, "api/bias.html",{
+                    "author": output[0],
+                    "title": output[1],
+                    "text": output[2],
+                    "news": news,
+                    "score": score
+                })
